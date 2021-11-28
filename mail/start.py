3370b87d0c0b121406d7aae1b680e9b1b00fe057
W29F <81409844+W29F@users.noreply.github.com>
2021-11-28 20:12:46 +0700

    MailXMusicVideo

--- mail/start.py
+++ mail/start.py
@@ -11,7 +11,7 @@ from config import (
     OWNER_NAME,
     UPDATES_CHANNEL,
 )
-from program import __version__
+from mail import __version__
 from driver.mail import user
 from driver.filters import command, other_filters
 from pyrogram import Client, filters
